e4b2b7548759c646c8bea326eceb907a74836405
Zaid Pathan <zaidkhanintel@gmail.com>
2019-03-24 21:46:39 +0530

    Initial viewcontroller setup.

---
+++ Shop/ShopModule/ShopModule.h
@@ -0,0 +1,19 @@
+//
+//  ShopModule.h
+//  ShopModule
+//
+//  Created by Zaid Pathan on 24/03/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShopModule.
+FOUNDATION_EXPORT double ShopModuleVersionNumber;
+
+//! Project version string for ShopModule.
+FOUNDATION_EXPORT const unsigned char ShopModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShopModule/PublicHeader.h>
+
+
